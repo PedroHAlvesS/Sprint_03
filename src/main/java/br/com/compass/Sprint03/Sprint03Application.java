cc5b5023a723adb6d4e318652455e44fb690e494
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-06-29 18:32:36 -0300

    initial files

---
+++ src/main/java/br/com/compass/Sprint03/Sprint03Application.java
@@ -0,0 +1,13 @@
+package br.com.compass.Sprint03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint03Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/Sprint03/Sprint03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.Sprint03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
