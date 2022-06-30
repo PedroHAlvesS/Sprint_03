2795d145600ff8df32f3720796f359119633f10e
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-06-29 21:21:51 -0300

    Update StateController.java

--- src/main/java/br/com/compass/Sprint03/controller/StateController.java
+++ src/main/java/br/com/compass/Sprint03/controller/StateController.java
@@ -11,13 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
